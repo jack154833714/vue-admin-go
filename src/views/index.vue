799227c90dc8eb9bad0295fb2d9a84700fae6822
jack <wong18226879992@gmail.com>
2024-05-15 00:37:36 +0700

    提交

--- src/views/index.vue
+++ src/views/index.vue
@@ -1,4 +1,10 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+defineOptions({
+  name: 'viewPage'
+})
+</script>
 
-<template><div>首页</div></template>
+<template>
+  <div>我的信息</div>
+</template>
 <style></style>
